7522356b7130e259f16f72fcd66fb25f21f54b1d
David Ison <developer@dtison.net>
2018-02-13 17:08:42 -0500

    Adds other member info input fields.

--- src/js/components/MemberInfo.js
+++ src/js/components/MemberInfo.js
@@ -52,48 +52,190 @@ export default class Login extends React.Component {
                                     <span className="verdanal10">The Info Below Needs To Match Your Credit Card Billing Statement. </span></div></td>
                             </tr>
                             <tr height="20">
-                                <td bgcolor="#FFFFFF" width="386" height="20">
-                                    <div align="left">&nbsp;
-                                    <i>First Name:&nbsp;</i>
+                            <td bgcolor="#FFFFFF" width="386" height="20">
+                                <div align="left">&nbsp;
+                                <i>First Name:&nbsp;</i>
+                                <b>
+                                <input name="FIRST_NAME" type="text" defaultValue={this.props.member.first_name}
+                                onChange={event=>{
+                                //    console.log("MemberInfo updating state.member was", this.state.member);
+                                    this.updateMemberInfo('first_name', event.target.value);
+                                }}
+                                size="20" maxLength="50"/>
+                                </b>
+                            </div>
+                            </td>
+                            </tr>
+
+                            <tr height="20">
+                            <td bgcolor="#FFFFFF" width="386" height="20">
+                                <div align="left">&nbsp;
+                                <i>Last Name:&nbsp;</i>
+                                <b>
+                                <input name="LAST_NAME" type="text" defaultValue={this.props.member.last_name}
+                                       onChange={event=>{
+                                           this.updateMemberInfo('last_name', event.target.value);
+                                       }}
+                                       size="20" maxLength="50"/>
+                                </b>
+                                </div>
+                            </td>
+                            </tr>
+
+                            <tr height="20">
+                            <td bgcolor="#FFFFFF" width="386" height="20">
+                                <div align="left">&nbsp;
+
+                                    <i>Address 1:&nbsp;</i>
                                     <b>
-                                    <input name="FIRST_NAME" type="text" defaultValue={this.props.member.first_name}
-                                    onChange={event=>{
-                                    //    console.log("MemberInfo updating state.member was", this.state.member);
-                                        this.updateMemberInfo('first_name', event.target.value);
-                                    }}
-                                    size="20" maxLength="50"/>
+                                    <input name="ADDRESS1" type="text" defaultValue={this.props.member.address1}
+                                           onChange={event=>{
+                                               this.updateMemberInfo('address1', event.target.value);
+                                           }}
+                                           size="30" maxLength="50"/>
                                     </b>
                                 </div>
+                            </td>
+                            </tr>
+
+                            <tr height="20">
+                            <td bgcolor="#FFFFFF" width="386" height="20">
+                            <div align="left">&nbsp;
+                            <i>Address 2:&nbsp;</i>
+                            <b>
+                            <input name="ADDRESS2" type="text" defaultValue={this.props.member.address2}
+                                   onChange={event=>{
+                                       this.updateMemberInfo('address2', event.target.value);
+                                   }}
+                                   size="30" maxLength="50"/>
+                            </b>
+                            </div>
+                            </td>
+                            </tr>
+
+                            <tr height="20">
+                                <td bgcolor="#FFFFFF" width="386" height="20">
+                                    <div align="left">&nbsp;
+                                        <i>City:&nbsp;</i>
+                                        <b>
+                                            <input name="CITY" type="text" defaultValue={this.props.member.city}
+                                                   onChange={event=>{
+                                                       this.updateMemberInfo('city', event.target.value);
+                                                   }}
+                                                   size="20" maxLength="30"/>
+                                        </b>
+                                    </div>
                                 </td>
                             </tr>
 
                             <tr height="20">
                                 <td bgcolor="#FFFFFF" width="386" height="20">
                                     <div align="left">&nbsp;
-
-                                        <i>Last Name:&nbsp;</i>
+                                        <i>State:&nbsp;</i>
                                         <b>
-                                            <input name="LAST_NAME" type="text" defaultValue={this.props.member.last_name}
+                                            <input name="STATE" type="text" defaultValue={this.props.member.state}
                                                    onChange={event=>{
-                                                       this.updateMemberInfo('last_name', event.target.value);
+                                                       this.updateMemberInfo('state', event.target.value);
                                                    }}
-                                                   size="20" maxLength="50"/>
-                                        </b></div>
+                                                   size="2" maxLength="2"/>
+                                        </b>
+                                    </div>
                                 </td>
                             </tr>
 
+                            <tr height="20">
+                                <td bgcolor="#FFFFFF" width="386" height="20">
+                                    <div align="left">&nbsp;
+                                        <i>Zip / Postal:&nbsp;</i>
+                                        <b>
+                                            <input name="POSTAL" type="text" defaultValue={this.props.member.postal}
+                                                   onChange={event=>{
+                                                       this.updateMemberInfo('postal', event.target.value);
+                                                   }}
+                                                   size="10" maxLength="10"/>
+                                        </b>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr height="20">
+                                <td bgcolor="#FFFFFF" width="386" height="20">
+                                    <div align="left">&nbsp;
+                                        <i>Country:&nbsp;</i>
+                                        <b>
+                                            <input name="COUNTRY" type="text" defaultValue={this.props.member.country}
+                                                   onChange={event=>{
+                                                       this.updateMemberInfo('country', event.target.value);
+                                                   }}
+                                                   size="6" maxLength="10"/>
+                                        </b>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr height="20">
+                                <td bgcolor="#FFFFFF" width="386" height="20">
+                                    <div align="left">&nbsp;
+                                        <i>Day Phone:&nbsp;</i>
+                                        <b>
+                                            <input name="DAY_PHONE" type="text" defaultValue={this.props.member.day_phone}
+                                                   onChange={event=>{
+                                                       this.updateMemberInfo('day_phone', event.target.value);
+                                                   }}
+                                                   size="12" maxLength="16"/>
+                                        </b>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr height="20">
+                                <td bgcolor="#FFFFFF" width="386" height="20">
+                                    <div align="left">&nbsp;
+                                        <i>Evening Phone:&nbsp;</i>
+                                        <b>
+                                            <input name="EVE_PHONE" type="text" defaultValue={this.props.member.eve_phone}
+                                                   onChange={event=>{
+                                                       this.updateMemberInfo('eve_phone', event.target.value);
+                                                   }}
+                                                   size="12" maxLength="16"/>
+                                        </b>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr height="20">
+                                <td bgcolor="#FFFFFF" width="386" height="20">
+                                    <div align="left">&nbsp;
+                                        <i>Email:&nbsp;</i>
+                                        <b>
+                                            <input name="EMAIL" type="text" defaultValue={this.props.member.email}
+                                                   onChange={event=>{
+                                                       this.updateMemberInfo('email', event.target.value);
+                                                   }}
+                                                   size="20" maxLength="50"/>
+                                        </b>
+                                    </div>
+                                </td>
+                            </tr>
                             <tr>
                                 <td>
                                     <input type="button" value="Save" onClick={e=>{
                                         PicksAPI.saveMember(this.state.member).done((res)=>{
-                                         //   console.log("SAVE done..", res);
-                                            this.NotificationManager.info('Changes saved.');
+                                            this.NotificationManager.info('Your changes saved.');
 
                                         });
 
                                     }}/>
-                                    <NotificationContainer/>
                                 </td>
+                            </tr>
+
+
+
+
+                            <tr>
+                                <td>
+                                <NotificationContainer/>
+</td>
                             </tr>
 
                             </tbody>
@@ -119,12 +261,14 @@ export default class Login extends React.Component {
     <b/></b></div></td>
     </tr>
     <tr height="20">
-    <td bgcolor="white" width="386" height="20"><div align="left">&nbsp;<i>City:&nbsp;</i><b><input name="CITY" type="text" value="{CITY}" size="20" maxLength="30"/>
+    <td bgcolor="white" width="386" height="20"><div align="left">&nbsp;<i>City:&nbsp;</i><b>
+    <input name="CITY" type="text" value="{CITY}" size="20" maxLength="30"/>
     </b><i>&nbsp;&nbsp;State:&nbsp;</i><b><input name="STATE" type="text" value="{STATE}" size="2" maxLength="2"/>
     </b></div></td>
     </tr>
     <tr height="20">
-    <td bgcolor="white" width="386" height="20"><div align="left">&nbsp;<i>Zip:&nbsp;</i><b><input name="POSTAL" type="text" value="{POSTAL}" size="10" maxLength="10"/>
+    <td bgcolor="white" width="386" height="20"><div align="left">&nbsp;<i>Zip:&nbsp;</i><b>
+    <input name="POSTAL" type="text" value="{POSTAL}" size="10" maxLength="10"/>
     </b><i>&nbsp;&nbsp;Country:&nbsp;</i><b><input name="COUNTRY" type="text" value="{COUNTRY}" size="6" maxLength="12"/>
     </b></div></td>
     </tr>
