d8c9732246b504f8dcd60af5b7d8e2cda30865a1
David Ison <developer@dtison.net>
2018-03-12 12:45:30 -0400

    Removes client.min.js

--- webpack.common.js
+++ webpack.common.js
@@ -23,9 +23,5 @@ module.exports = {
     path: __dirname + "/src/",
     filename: "client.min.js"
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+
 };
---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    plugins: [
+        new UglifyJSPlugin()
+    ]
+});
+
+/*
+
+plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+],*/
