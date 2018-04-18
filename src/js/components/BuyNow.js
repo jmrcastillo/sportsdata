0751ecee39adb350bb448a6c40220db7f9c8366c
David Ison <developer@dtison.net>
2018-04-18 11:10:31 -0400

    Experimental css for login modal.

--- src/js/components/BuyNow.js
+++ src/js/components/BuyNow.js
@@ -17,12 +17,12 @@ export default class BuyNow extends React.Component {
 
         this.customStyles = {
             content : {
-                top                   : '50%',
+/*                top                   : '50%',
                 left                  : '50%',
                 right                 : 'auto',
                 bottom                : 'auto',
                 marginRight           : '-50%',
-                transform             : 'translate(-50%, -50%)'
+                transform             : 'translate(-50%, -50%)'*/
             }
         };
     }
